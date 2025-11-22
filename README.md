c201a18164633d82233fe5b0c12038d965d10f60
zakariadjellouli <djellouli.zakaria.zd@gmail.com>
2025-11-21 19:33:33 -0500

    etape 1 : commit de qst 7

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_app_navigation
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_app_navigation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_app_navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
